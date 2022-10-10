e37efe93bc5742adb27c66cfa35b3b6aec8424a1
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2022-10-10 16:50:34 +0200

    onject properties

--- js-resource/array.js
+++ js-resource/array.js
@@ -18,3 +18,23 @@ const check = (arr) => {
 }
 console.log(check([1,23,4,5]));
 console.log(check ('allene'));
+
+// to get first element of array
+ 
+const first = [1,2,3,4];
+const findFirst = first[0];
+console.log('first value',findFirst);
+
+const getFirst = () => {
+const get = ['java','html','css'];
+const firstValue = get.slice(0,1);
+console.log('the first value is',firstValue);
+}
+getFirst();
+
+const getFirs = () => {
+    const get = ['javaScript','html','css'];
+    const firstValue = get.shift();
+    console.log('the first value is',firstValue);
+    }
+    getFirs();
--- js-resource/string.js
+++ js-resource/string.js
@@ -3,3 +3,10 @@ const str1 = "world";
 console.log("str:", str)
 console.log('str1 = ',str1)
 
+
+// assign property object to object
+
+const student = {};
+student["name"] = 'ARC';
+student.lastName = 'tutoliar';
+console.log(student);
