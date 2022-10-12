386fffc9ce43579e01717fa64e060270a99a53f8
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2022-10-12 11:32:39 +0200

    to check integer

--- js-resource/array.js
+++ js-resource/array.js
@@ -64,3 +64,9 @@ const getFirs = () => {
       }
       
       testLogicalOr(15);
+
+      function duplicate(arr) {
+        return arr.concat(arr);
+      }
+      
+      console.log(duplicate([1, 2, 3, 4, 5]));
