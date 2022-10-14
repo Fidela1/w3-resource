1ad009e08e616b51e360f17a4de4aa939d636437
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2022-10-14 18:26:13 +0200

    add some string methods

--- js-resource/string.js
+++ js-resource/string.js
@@ -72,3 +72,67 @@ let text1 = 'Computer';
 let text2 = 'Science';
 let text3 = text1.concat('',text2);
 console.log(text3);
+
+// trim method used to remove space from both side of string
+
+let sentence = "    Hello World    ";
+let sentence1 = sentence.trim();
+console.log(sentence1);
+
+// trimEnd remove space on the end of string
+let sentence2 = "    Hello World    ";
+let sentence3 = sentence2.trimEnd();
+console.log(sentence3);
+
+//// padding method
+// padding start
+ let num1 = "6";
+ let num2 = num1.padStart(4,"x");
+ console.log(num2); // xxx6
+ 
+
+ let num3 = "6";
+ let num4 = num3.padStart(4,"0");
+ console.log(num4); //0006
+
+ // padding end
+ let num5 = "6";
+ let num6 = num5.padEnd(4,"6");
+ console.log(num6); // 6666
+
+
+ let num7 = "6";
+ let num8 = num7.padEnd(4,"0"); 
+ console.log(num8);  // 6000
+
+ // charAt() method return character of specified position
+
+ let snt = "Hello World";
+ let snt1 = snt.charAt(0);
+console.log(snt1); // H
+
+// indexOf method return index of element
+
+let snt3 = "Hello World";
+ let snt4 = snt3.indexOf("World");
+console.log(snt4); // 6
+
+let snt5 = " Hello World";
+ let snt6 = snt5.lastIndexOf("World");
+console.log(snt6); // 7
+
+// include return true if element is included
+
+let txt = "Hello world, welcome to the universe.";
+let txt1 = txt.includes("welcome");
+console.log(txt1); // true
+
+let txt2 = "Hello world, welcome to the universe.";
+let txt3 = txt2.includes("fi");
+console.log(txt3); // false
+
+// startsWith method
+
+let nm = "Hello World";
+let nm1 = nm.startsWith("Hello");
+console.log(nm1);  // tru becouse it started by hello
