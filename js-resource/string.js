878bc0cfe88c457f81be71bba61c423277cc993b
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2022-11-05 12:41:32 +0200

    find shortest word

--- js-resource/string.js
+++ js-resource/string.js
@@ -467,3 +467,15 @@ for(xi=1; xi <=6; xi++)
  console.log(chr);
  chr='';    
     }
+
+    // find the shortest word in a string
+
+    function findShortestWord(str) {
+        var words = str.split(' ');
+        var shortest = words.reduce((shortestWord, currentWord) => {
+          return currentWord.length < shortestWord.length ? currentWord : shortestWord;
+        }, words[0]);
+        return shortest;
+      }
+      console.log(findShortestWord("The quick brown fox jumped over the lazy dog"));
+      
