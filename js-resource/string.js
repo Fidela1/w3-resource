cf154ce69142e57754a7d75d7ff181da8a0ecd4b
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2022-11-11 23:44:13 +0200

    find sum

--- js-resource/string.js
+++ js-resource/string.js
@@ -533,3 +533,15 @@ function consonants(str) {
   
   substrings("dog");
   
+
+// Find the sum of squares of a numeric vector
+
+function sum_sq(array) {
+    var sum = 0, 
+        i = array.length;
+    while (i--) 
+     sum += Math.pow(array[i], 2);
+    return sum;
+  }
+   
+  console.log(sum_sq([0,1,2,3,4])); 
