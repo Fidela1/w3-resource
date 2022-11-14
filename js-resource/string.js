242f6b2f597a585d36791eff6b5fde499b6494b6
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2022-11-14 23:16:12 +0200

    new to commit

--- js-resource/string.js
+++ js-resource/string.js
@@ -556,3 +556,14 @@ function sum_sq(array) {
   console.log(is_array('w3resource'));
   console.log(is_array([1, 2, 4, 0]));
   
+//   Concatenate a specific string for a specific number of times
+
+  repeat = function repeat(str, count) {
+    if(typeof(count) == "undefined") {
+    count =1;
+  }
+  return count < 1 ? '' : new Array(count + 1).join(str);
+    }
+console.log(repeat('Ha!'));
+console.log(repeat('Ha!',2));
+console.log(repeat('Ha!',3));
